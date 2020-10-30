0ce5068a1fb8169c99cf1ce22787c05f9bcf5d33
giuliam11 <73106999+giuliam11@users.noreply.github.com>
2020-10-30 09:39:53 +0100

    Create page.js

---
+++ page.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    // Open in new window
+        $('.click').each(function() {
+        var link = $(this).html();
+        $(this).contents().wrap('<a href="google.com"></a>');
+        });
+});
+
+// Make Card One Clickable
+function projectOne() {
+    window.open;
+}
+
+// Make Card Two Clickable
+function projectTwo() {
+    window.open;
+}
+
+// Make Card Three Clickable
+function projectThree() {
+    window.open;
+}
